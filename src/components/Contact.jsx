b167722a9b224691bd84c45c81d4de4b63a63d5f
suiunpalk <suiunpalk@gmail.com>
2023-11-10 17:48:30 +0900

    수정 2.2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,16 +7,8 @@ const Contact = () => {
         <section id="contact">
             <div className="contact__inner">
                 <h2 className="contact__title">Contact</h2>
-                <div className="contact__lines top" aria-hidden="true">
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                    <span className="line"></span>
-                </div>
-                <div className="contact__text">
+              
+                <div className="contact__text gradient">
                     <div className="text">
                         {contactText.map((contact, key) => (
                             <div key={key}>
