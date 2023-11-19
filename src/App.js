07efbcd233a2e6cf2b9fc6807d17663eeb8bf49d
suiunpalk <suiunpalk@gmail.com>
2023-11-19 16:04:47 +0900

    수정 3.7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
 import HomeView from "./views/HomeView";
-import QuoteView from "./views/QuoteView";
+// import QuoteView from "./views/QuoteView";
 import smooth from "./utils/smooth";
 import link from "./utils/link"
 
@@ -16,7 +16,7 @@ const App = () => {
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomeView />} />
-            <Route path="/contact" element={<QuoteView />} />           
+            {/* <Route path="/contact" element={<QuoteView />} />            */}
         </Routes>
     </BrowserRouter>
     
